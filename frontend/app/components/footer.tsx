d18e0c5cfe0b6cf2b8015ddbb273962ba25b6b52
Bhumit Singh <chiragsingh360@gmail.com>
2025-03-11 20:19:20 +0530

    new changes

---
+++ frontend/app/components/footer.tsx
@@ -0,0 +1,5 @@
+import Link from "next/link";
+
+export default function Footer() {
+    return <footer className="text-center text-gray-600 py-4">© 2025 Elementary</footer>;
+}
---
+++ frontend/app/home/page.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "@/app/components/sidebar";
+import Footer from "@/app/components/footer";
+
+export default function Home() {
+    return (
+        <>
+            <Sidebar />
+            <Footer />
+        </>
+    )
+}
